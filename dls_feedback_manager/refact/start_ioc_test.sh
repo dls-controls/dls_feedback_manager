146a1beaa0a202abde1e2762a6e7f8bb887c0bc1
Niamh Dougan <niamh.dougan@diamond.ac.uk>
2018-03-29 13:06:24 +0100

    removed pyioc params

--- dls_feedback_manager/refact/start_ioc_test.sh
+++ dls_feedback_manager/refact/start_ioc_test.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-PYIOC_VERSION=2-11
-PYIOC=/dls_sw/prod/R3.14.12.3/support/pythonSoftIoc/$PYIOC_VERSION/pythonIoc
 export EPICS_CA_SERVER_PORT=6164
 export EPICS_CA_REPEATER_PORT=6165
 export EPICS_CA_ADDR_LIST=127.0.0.1
