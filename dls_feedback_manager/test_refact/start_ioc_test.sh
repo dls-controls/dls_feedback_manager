30d8a4fc859a53fc72b590944ad293776228c867
Niamh Dougan <niamh.dougan@diamond.ac.uk>
2018-04-04 15:49:52 +0100

    exec test

--- dls_feedback_manager/test_refact/start_ioc_test.sh
+++ dls_feedback_manager/test_refact/start_ioc_test.sh
@@ -10,11 +10,11 @@
     fi '
 
 PYIOC_VERSION=2-11
-PYIOC=$TRAVIS_BUILD_DIR/pythonIoc
+#PYIOC=$TRAVIS_BUILD_DIR/pythonIoc
 export EPICS_CA_SERVER_PORT=6164
 export EPICS_CA_REPEATER_PORT=6165
 export EPICS_CA_ADDR_LIST=127.0.0.1
 #export EPICS_CA_AUTO_ADDR_LIST=NO
 
 cd "$(dirname "$0")"
-exec $PYIOC test_manager.py "$@"
+exec test_refact/test_manager.py "$@"
