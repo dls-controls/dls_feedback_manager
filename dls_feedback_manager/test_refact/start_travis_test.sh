bd49430ee23c3a0780e8560b3909132c2c58fc64
Niamh Dougan <niamh.dougan@diamond.ac.uk>
2018-04-05 12:01:23 +0100

    back to the original

--- dls_feedback_manager/test_refact/start_travis_test.sh
+++ dls_feedback_manager/test_refact/start_travis_test.sh
@@ -3,10 +3,11 @@
 # Running in travis
 
 PYIOC_VERSION=2-11
-PYIOC=pythonSoftIoc-2.11/pythonIoc
-EPICS_CA_SERVER_PORT="6164"
-EPICS_CA_REPEATER_PORT="6165"
-EPICS_CA_ADDR_LIST="127.0.0.1"
+PYIOC=$TRAVIS_BUILD_DIR/pythonSoftIoc/2-11/pythonIoc
+export EPICS_CA_SERVER_PORT=6164
+export EPICS_CA_REPEATER_PORT=6165
+export EPICS_CA_ADDR_LIST=127.0.0.1
 #export EPICS_CA_AUTO_ADDR_LIST=NO
 
-exec PYIOC test_manager
+cd "$(dirname "$0")"
+exec $PYIOC test_manager.py "$@"
