95606ca3d8b1c61c26e910ffc3abbd8939bd520c
Niamh Dougan <niamh.dougan@diamond.ac.uk>
2018-04-05 11:22:47 +0100

    removed ... because of syntax error

--- dls_feedback_manager/test_refact/start_travis_test.sh
+++ dls_feedback_manager/test_refact/start_travis_test.sh
@@ -3,7 +3,7 @@
 # Running in travis
 
 PYIOC_VERSION=2-11
-PYIOC=$TRAVIS_BUILD_DIR/pythonSoftIoc-2.11/pythonIoc
+PYIOC=pythonSoftIoc-2.11/pythonIoc
 export EPICS_CA_SERVER_PORT=6164
 export EPICS_CA_REPEATER_PORT=6165
 export EPICS_CA_ADDR_LIST=127.0.0.1
