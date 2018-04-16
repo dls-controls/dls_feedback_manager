43d021d4dc5e0fc5a571534c3d5487272b8754a9
Niamh Dougan <niamh.dougan@diamond.ac.uk>
2018-04-16 09:52:39 +0100

    pre doxygen formatting

--- dls_feedback_manager/test_refact/XBPM_test_control.py
+++ dls_feedback_manager/test_refact/XBPM_test_control.py
@@ -9,24 +9,32 @@ require('epicsdbbuilder==1.2')
 import unittest
 
 import test_range_manager
-test_range = test_range_manager.RangeTests()
-
 import test_feedback_manager
+test_range = test_range_manager.RangeTests()
+test_feedback = test_feedback_manager.FeedbackTests()
 
-XBPMTestSuite = unittest.TestSuite()
+if __name__ == "__main__":
+    unittest.TestLoader().discover('.')
+    unittest.TextTestRunner(verbosity=2).run('test*.py')
 
-def suite():
-    tests = ['setUp,', 'test_standard_naming', 'test_regular_expression',
-          'test_initial_variable_inputs', 'test_invalid_threshold_percentage', 'tearDown']
 
-    return unittest.TestSuite(map(test_range, tests))
+# XBPMTestSuite = unittest.TestSuite()
+
+#def suite():
+ #  tests = ['setUp,', 'test_standard_naming', 'test_regular_expression',
+
+  #      'test_initial_variable_inputs', 'test_invalid_threshold_percentage', 'tearDown']
+
+   #return unittest.TestSuite(map(test_range, tests))
 
 #  Run tests for XBPM range manager
 
+#if __name__ == "main":
+ #   unittest.TestLoader().loadTestsFromModule(test_range_manager)
+  #  unittest.TextTestRunner(verbosity=2).run('.')
 
-
+#suite = unittest.TestLoader().loadTestsFromTestCase(test_range_manager.RangeTests)
+#unittest.TextTestRunner(verbosity=2).run(suite)
 
 # Run tests for XBPM feedback manager
-
-test_feedback = test_feedback_manager.FeedbackTests()
 
